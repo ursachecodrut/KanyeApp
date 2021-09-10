9bae189ec1f838e326e680abadac2c5c6f0c9206
ursachecodrut <ursache.codrut71@gmail.com>
2021-09-10 22:45:06 +0300

    fix github actions & add function that prints random kanye quote in console

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String? _quote;
+  final String _apiUrl = 'http://api.kanye.rest/';
+  final Client _client = Client();
+
+  Future<void> getKanyeQuote() async {
+    final Uri uri = Uri.parse(_apiUrl);
+    final Response response = await _client.get(uri);
+
+    if (response.statusCode >= 300) {
+      throw StateError(response.body);
+    }
+
+    final Map<String, dynamic> mapQuote = jsonDecode(response.body) as Map<String, dynamic>;
+    _quote = mapQuote['quote'] as String;
+    print(_quote);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          child: const Center(
-            child: Text('test'),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              getKanyeQuote();
+            },
+            child: const Text('print Kanye'),
           ),
         ),
       ),
